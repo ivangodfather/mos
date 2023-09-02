764e650960913c4a0e64b40c483fe04d5e73c7ba
Ivan Ruiz <ruiz@fueled.com>
2023-09-02 13:09:50 +0200

    Initial Commit

---
+++ MomentsOfSpace/MomentsOfSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentsOfSpaceApp.swift
+//  MomentsOfSpace
+//
+//  Created by Ivan on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MomentsOfSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
